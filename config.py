4ce86d3a4773c7f483397943b7099b0427bd7a86
Zeomite <105901968+Zeomite@users.noreply.github.com>
2023-05-06 21:35:24 +0530

    Update config.py
    encrypted the passwords

--- config.py
+++ config.py
@@ -1,17 +1,1 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('AIzaSyCTFoHJPf2913TzKmpgaAou97tiYsB_B-0')
-    FIREBASE_CREDENTIALS = 'static/js/app.json'
-    FIREBASE_DATABASE_URL = 'https://car-track-app-a01ae-default-rtdb.firebaseio.com'
-
-config={
-  "apiKey": "AIzaSyCTFoHJPf2913TzKmpgaAou97tiYsB_B-0",
-  "authDomain": "car-track-app-a01ae.firebaseapp.com",
-  "databaseURL": "https://car-track-app-a01ae-default-rtdb.firebaseio.com/",
-  "projectId": "car-track-app-a01ae",
-  "storageBucket": "car-track-app-a01ae.appspot.com",
-  "messagingSenderId": "1068935463054",
-  "appId": "1:1068935463054:web:d40190a737674ee2c849df",
-  "measurementId": "G-T71BE3D16C"
-};
+"This file has been encrypted for security reasons"
