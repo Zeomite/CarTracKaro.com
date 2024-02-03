34a2abe5483836546a36813e8073a102f9361223
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-02-03 14:27:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os 
 config = {
     'apiKey': os.environ.get('API_KEY'),
-    'authDomain': os.environ.get('YOUR_PROJECT_ID.firebaseapp.com'),
-    'databaseURL': os.environ.get('https://YOUR_PROJECT_ID.firebaseio.com'),
+    'authDomain': os.environ.get('YOUR_PROJECT_ID_firebaseapp_com'),
+    'databaseURL': os.environ.get('YOUR_PROJECT_ID_firebaseio_com'),
     'projectId': os.environ.get('YOUR_PROJECT_ID'),
-    'storageBucket': os.environ.get('YOUR_PROJECT_ID.appspot.com'),
+    'storageBucket': os.environ.get('YOUR_PROJECT_ID_appspot_com'),
     'messagingSenderId': os.environ.get('YOUR_MESSAGING_SENDER_ID'),
     'appId': os.environ.get('YOUR_APP_ID'),
     'measurementId': os.environ.get('YOUR_MEASUREMENT_ID)'
