3024f9b7bf87f9091768b2021d966a62339643e2
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-02-04 16:05:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-import os 
-config = {
-    'apiKey': os.environ.get('API_KEY'),
-    'authDomain': os.environ.get('YOUR_PROJECT_ID_firebaseapp_com'),
-    'databaseURL': os.environ.get('YOUR_PROJECT_ID_firebaseio_com'),
-    'projectId': os.environ.get('YOUR_PROJECT_ID'),
-    'storageBucket': os.environ.get('YOUR_PROJECT_ID_appspot_com'),
-    'messagingSenderId': os.environ.get('YOUR_MESSAGING_SENDER_ID'),
-    'appId': os.environ.get('YOUR_APP_ID'),
-    'measurementId': os.environ.get('YOUR_MEASUREMENT_ID')
+import os
+config={
+    "type": os.environ.get('TYPE'),
+    "project_id": os.environ.get('PROJECT_ID'),
+    "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+    "private_key": os.environ.get('PRIVATE_KEY'),
+    "client_email": os.environ.get('CLIENT_EMAIL'),
+    "client_id": os.environ.get('CLIENT_ID'),
+    "auth_uri": os.environ.get('AUTH_URI'),
+    "token_uri": os.environ.get('TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL')
 }
